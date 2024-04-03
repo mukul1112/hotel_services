3fcc304e2a247fa9606769decc366a2416223416
mukul112 <mkpalariya1112@gmail.com>
2024-04-03 17:04:57 +0530

    prod deploy

--- src/main/java/com/lcwd/hotel/HotelServices/HotelServicesApplication.java
+++ src/main/java/com/lcwd/hotel/HotelServices/HotelServicesApplication.java
@@ -2,10 +2,10 @@ package com.lcwd.hotel.HotelServices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
-@EnableEurekaServer
+
 public class HotelServicesApplication {
 
 	public static void main(String[] args) {
